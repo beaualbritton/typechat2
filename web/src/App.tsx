71d493da5270aa2caf7e526d20b2b2b34a55874b
beau albritton <albrittoncb@gmail.com>
2026-01-01 02:22:20 -0500

    feat: basic styling for web w/ everforst css
    css from valen

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState, type FormEvent } from 'react'
 import { useClient } from './lib/hooks/useClient.ts';
-import './App.css'
 
 function App() {
 
@@ -20,27 +19,28 @@ function App() {
   if(!connected)
   {
     return(
-      <div className="flex flex-col">
-        <p>Enter A Username: </p>
-        <form onSubmit={(e) => {e.preventDefault(); setConnection(true);}}>
-          <input value={username} onChange={(e) => setUsername(e.target.value)}/>
-          <button type="submit">Connect</button>
-        </form>
-      </div>
+      <main className="flex flex-col h-screen w-full items-center justify-center">
+        <div className="p-4 bg-elevated w-2xl h-96 rounded border-main border-2 flex flex-col items-center justify-center">
+          <h1 className="text-5xl text-green font-bold p-2">Enter Username</h1>
+          <form className="w-2xl flex flex-col items-center justify-center p-4" onSubmit={(e) => {e.preventDefault(); setConnection(true);}}>
+            <input className="bg-surface text-xl border border-main text-yellow rounded w-full p-4" value={username} onChange={(e) => setUsername(e.target.value)}/>
+            <button className="bg-surface text-xl border border-main text-green mt-4 p-4 rounded active:brightness-90 hover:brightness-110" type="submit">Connect</button>
+          </form>
+        </div>
+      </main>
     )
   }
 
   return (
-  <div className="flex flex-col">
-    <div>
-      {messages.map((msg:string, i:number) => (<p key={i}>{msg}</p>))}
-    </div>
-
-    <form onSubmit={submit}>
-      &gt;&nbsp; <input value={input} onChange={(e) => setInput(e.target.value)}/>
-      <button type="submit">Send</button>
-    </form>
-  </div>
+    <main className="flex flex-col h-screen w-full items-center justify-center">
+      <div className="p-4 bg-elevated w-2xl rounded border-main border-2 flex flex-col overflow-y-auto mb-4">
+        {messages.map((msg:string, i:number) => (<p key={i} className="text-yellow p-2">{msg}</p>))}
+      </div>
+      <form className="w-2xl flex items-center bg-surface border border-main rounded p-4" onSubmit={submit}>
+        <span className="text-green text-xl">&gt;&nbsp;</span> <input className="bg-surface text-xl text-yellow flex-1 outline-none" value={input} onChange={(e) => setInput(e.target.value)}/>
+        <button className="bg-elevated text-xl border border-main text-green ml-4 px-4 py-2 rounded active:brightness-90 hover:brightness-110" type="submit">Send</button>
+      </form>
+    </main>
   );
 };
 
